4b66853fdf916795b1a8ad208647bd3f88045fc5
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-12-08 11:28:23 +0000

    First working version
    Auth is disabled, NATS is disabled, metrics are disabled,
    scaling is disabled.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- src/charm.py
+++ src/charm.py
@@ -4,12 +4,14 @@
 
 import logging
 
-from pathlib import Path
 
 from ops.charm import CharmBase
 from ops.main import main
 from ops.framework import StoredState
 from ops.model import ActiveStatus
+import os
+
+from pathlib import Path
 import yaml
 import json
 
@@ -20,10 +22,12 @@ class OpenfaasCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+        self._stored.set_default(namespace=os.environ["JUJU_MODEL_NAME"])
         self.framework.observe(self.on.config_changed, self._on_config_changed)
 
     def _on_config_changed(self, _=None): 
         logger.debug("config_change")
+        print("config_change")
 
         if not self.unit.is_leader():
             self.unit.status = ActiveStatus()
@@ -34,49 +38,61 @@ class OpenfaasCharm(CharmBase):
         self.unit.status = ActiveStatus("OpenFaaS pod ready.")
 
     def _build_pod_spec(self):
-        namespace = self.model.name
+        namespace = self._stored.namespace
 
-        function_crd = {}
-        profiles_crd = {}
+        # function_crd = {}
+        # profiles_crd = {}
 
+        rules = []
         try:
-            function_crd = yaml.load(open(Path('files/function_crd.yaml'),"r"), Loader=yaml.FullLoader)
+            rules = yaml.load(open(Path('files/rbac_rules.yaml'),"r"), Loader=yaml.FullLoader)
         except yaml.YAMLError as exc:
             print("Error in configuration file:", exc)
 
-        try:
-            profiles_crd = yaml.load(open(Path('files/profiles_crd.yaml'),"r"), Loader=yaml.FullLoader)
-        except yaml.YAMLError as exc:
-            print("Error in configuration file:", exc)
+        # try:
+        #     function_crd = yaml.load(open(Path('files/function_crd.yaml'),"r"), Loader=yaml.FullLoader)
+        # except yaml.YAMLError as exc:
+        #     print("Error in configuration file:", exc)
 
-        logger.debug(json.dumps(function_crd["spec"]))
+        # try:
+        #     profiles_crd = yaml.load(open(Path('files/profiles_crd.yaml'),"r"), Loader=yaml.FullLoader)
+        # except yaml.YAMLError as exc:
+        #     print("Error in configuration file:", exc)
+
+        # logger.debug(json.dumps(function_crd["spec"]))
 
 # "functions_provider_url": "http://192.168.0.35:8080",
         spec = {
             "version": 3,
             "kubernetesResources": {
-                "customResourceDefinitions": [
-                    {
-                        "name": function_crd["metadata"]["name"],
-                        "labels": {
-                            "juju-global-resource-lifecycle": "model",
-                        },
-                        "spec": function_crd["spec"],
-                    },
-                    {
-                        "name": profiles_crd["metadata"]["name"],
-                        "labels": {
-                            "juju-global-resource-lifecycle": "model",
-                        },
-                        "spec": profiles_crd["spec"],
-                    },
-                ],
+                # "customResourceDefinitions": [
+                #     {
+                #         "name": function_crd["metadata"]["name"],
+                #         "labels": {
+                #             "juju-global-resource-lifecycle": "model",
+                #         },
+                #         "spec": function_crd["spec"],
+                #     },
+                #     {
+                #         "name": profiles_crd["metadata"]["name"],
+                #         "labels": {
+                #             "juju-global-resource-lifecycle": "model",
+                #         },
+                #         "spec": profiles_crd["spec"],
+                #     },
+                # ],
+            },
+            'serviceAccount': {
+                'roles': [{
+                    'global': True,
+                    'rules': rules["rules"],
+                }],
             },
             "containers": [
                 {
                     "name": self.app.name+"-gateway",
                     "imageDetails": {"imagePath": "openfaas/gateway:0.20.2"},
-                    "ports": [{"containerPort": 8080, "protocol": "TCP"}],
+                    "ports": [{"containerPort": 8080, "protocol": "TCP","name":"gateway"}],
                     "envConfig": {
                         "functions_provider_url": "http://127.0.0.1:8081",
                         "direct_functions": "false",
@@ -87,14 +103,16 @@ class OpenfaasCharm(CharmBase):
                 },
                 {
                     "name": self.app.name+"-provider",
-                    "imageDetails": {"imagePath": "openfaas/operator:0.12.9"},
-                    "ports": [{"containerPort": 8081, "protocol": "TCP"}],
+                    "imageDetails": {"imagePath": "ghcr.io/openfaas/faas-netes:0.12.9"},
+                    "ports": [{"containerPort": 8081, "protocol": "TCP","name":"provider"}],
                     "envConfig": {
                         "port": "8081",
+                        "operator": "true",
                         "function_namespace": namespace,
-                        "cluster_role": "false",
+                        "cluster_role": "true",
                         "profiles_namespace": namespace
-                    }
+                    },
+                    "command": ["./faas-netes","-operator=true"],
                 }
             ]
         }
